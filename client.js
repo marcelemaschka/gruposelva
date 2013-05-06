23ac22b7737ab364ff0d56b775943467e21b553f
Marcele Maschka <marcele.maschka@gmail.com>
2013-05-06 11:44:37 -0300

    faafafsd

--- client.js
+++ client.js
@@ -1,9 +1,13 @@
 var extraParser = /(\d+)\/(\d+)/;
 
 onChange = function(property, newValue, oldValue, attributes) {
-  var escopo, qtd, dias, match, cargo, material;
-  var total = 0;
+  var escopo, qtd, dias, match, cargo, material, rv;
   if (property === 'escopo' && newValue) {
+    rv = {
+      values: {
+        preco_final: 0,
+      }
+    };
     escopo = getLocal('escopo', newValue._key).value;
     if (escopo.mao_obra) {
       for (var i=0; i < escopo.mao_obra.length; i++) {
@@ -16,17 +20,19 @@ onChange = function(property, newValue, oldValue, attributes) {
           throw new Error('dsd');
         }
         cargo = getLocal('cargos', escopo.mao_obra[i].item._key).value;
-        total += cargo.custo_diario * dias * qtd;
+        rv.values.preco_final += cargo.custo_diario * dias * qtd;
       }
+      rv.values.mao_obra = escopo.mao_obra;
     }
     if (escopo.material) {
       for (var i=0; i < escopo.material.length; i++) {
         qtd = parseInt(escopo.material[i].extraInfo);
         material = getLocal('material', escopo.material[i].item._key).value;
-        total += material.preco_de_venda * qtd;
+        rv.values.preco_final += material.preco_de_venda * qtd;
       }
+      rv.values.material = escopo.material;
     }
-    return {values: {preco_final: total}};
+    return rv;
   }
 };
 
