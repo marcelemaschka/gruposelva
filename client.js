9411267dd8e0093449136c92de52503a8f4dca42
marcele.maschka <marcele.maschka@gmail.com>
2013-05-08 17:13:53 -0300

    Inclusão do modulo com campos Pet e dara da consulta

---
+++ client.js
@@ -0,0 +1,13 @@
+onChange = function(property, newValue, oldValue, attributes) {
+  var nome_cliente = '', data = '';
+
+  if (property === 'cliente' || property === 'data_da_visita') {
+    if (attributes.cliente)
+      nome_cliente = attributes.cliente.nome_fantasia;
+    if (attributes.data_da_visita instanceof Date)
+      data = _.dateFormat(attributes.data_da_visita, 'dd/MM/yyyy');
+    return {values: {nome_cliente_data: nome_cliente + ', ' + data}};
+  }
+};
+
+
